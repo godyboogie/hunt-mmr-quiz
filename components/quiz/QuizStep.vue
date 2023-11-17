f8d4ea85da3d2e278b94ec941c6705b483f5126e
godyboogie <gamlo1197@hotmail.com>
2023-11-17 00:57:22 -0500

    start

--- components/quiz/QuizStep.vue
+++ components/quiz/QuizStep.vue
@@ -1,19 +1,42 @@
 <script setup>
-defineProps({
-  options: [],
+const props = defineProps({
+  options: Array,
   name: String,
   question: String,
   imageUrl: String,
   correctOption: String
 })
+
+const emit = defineEmits(['nextQuestion'])
+
+const optionsObject = ref(Object.assign({}, ...props.options.map((x) => ({[x.value]: null}))))
+const disabledAnswers = ref(false)
+
+const handleClick = (option) => {
+  disabledAnswers.value = true;
+  optionsObject.value[option.value] = option.isRight ? "btn_right" : "btn_wrong"
+
+  if (option.isRight) {
+    emit("nextQuestion", true)
+  } else {
+    emit("nextQuestion", false)
+  }
+}
 </script>
 
 <template>
   <div class="step">
-    <label v-for="option in options" :key="option.value" :for="option.value" class="radio">
-      <input type="radio" :value="option.value" :id="option.value" :name="name" class="radio-input">
-      <span class="radio-fakeInput"></span>
-      {{ option.name }}
-    </label>
+    <h2>{{ question }}</h2>
+    <button v-for="option in options" :key="option.value" :class="['btn', optionsObject[option.value]]" :disabled="disabledAnswers" @click="handleClick(option)">{{ option.name }}</button>
   </div>
 </template>
+
+<style lang="scss">
+.btn_right {
+  background: green;
+}
+
+.btn_wrong {
+  background: red;
+}
+</style>
