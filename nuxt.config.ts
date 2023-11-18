4ee5fc424f94bf47125f2906e2911c694d411a28
godyboogie <gamlo1197@hotmail.com>
2023-11-17 21:06:23 -0500

    deploy work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   app: {
     baseURL: '/hunt-star-quiz/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
