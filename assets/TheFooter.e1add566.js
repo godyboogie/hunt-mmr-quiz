29ed3a763fe237d2f31bf2b047987d7dc86c35c0
godyboogie <gamlo1197@hotmail.com>
2023-11-17 21:12:04 -0500

    Updates

---
+++ assets/TheFooter.e1add566.js
@@ -0,0 +1,1 @@
+import{_ as e,q as o,s as r}from"./entry.7363f37e.js";const t={};function c(s,n){return o(),r("footer",null,"@godyboogie - all rights reserved")}const _=e(t,[["render",c]]);export{_ as default};
