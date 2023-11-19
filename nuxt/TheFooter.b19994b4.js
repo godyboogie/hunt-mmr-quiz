102264674aa03d127d55d5c645b23b6869db3def
godyboogie <gamlo1197@hotmail.com>
2023-11-18 19:36:27 -0500

    Updates

--- nuxt/TheFooter.b19994b4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,q as o,s as r}from"./entry.7c0c51da.js";const t={};function c(s,n){return o(),r("footer",null,"@godyboogie - all rights reserved")}const _=e(t,[["render",c]]);export{_ as default};
--- nuxt/TheHeader.6f47dc97.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,q as r,s as c}from"./entry.7c0c51da.js";const a={};function n(t,o){return r(),c("header",null,"Header")}const _=e(a,[["render",n]]);export{_ as default};
